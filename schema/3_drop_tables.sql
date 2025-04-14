6c1776bc6be28e176cd9c311508089f602b9bd74
Sevgi Toprak <134732636+sevgitc@users.noreply.github.com>
2025-04-14 16:31:18 -0700

    Create 3_drop_tables.sql
    add drop script to reset database structure

---
+++ schema/3_drop_tables.sql
@@ -0,0 +1,14 @@
+USE AdswiseDB;
+
+DROP TABLE Person;
+DROP TABLE Advertisement;
+DROP TABLE BoardLocation;
+DROP TABLE NonCollegeMember;
+DROP TABLE Student_CollegeMember;
+DROP TABLE Professor_CollegeMember;
+DROP TABLE Staff_CollegeMember;
+DROP TABLE Person_Sends_Message;
+DROP TABLE Person_Receives_Message;
+DROP TABLE Advertisement_Message;
+DROP TABLE Staff_Approves_Advertisement;
+DROP TABLE Advertisement_LocatedOn_BoardLocation;
